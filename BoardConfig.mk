9f42e7289e9281aee170deaff3bfdc1f12ef1b26
vlad <vladioan3@gmail.com>
2015-08-22 01:35:59 +0300

    perfect boot,keyboard working,fix some sound libs

---
+++ BoardConfig.mk
@@ -0,0 +1,105 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/samsung/zanin/BoardConfigVendor.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_ARCH:=arm
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOOTLOADER_BOARD_NAME := zanin
+TARGET_BOARD_PLATFORM := rhea
+#BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8 mem=480M gpt pmem=112M carveout=18M androidboot.console=ttyS1
+BOARD_KERNEL_BASE := 0x82000000
+BOARD_KERNEL_PAGESIZE := 4096
+#COMMON_GLOBAL_CFLAGS += -DMR0_AUDIO_BLOB -DSAMSUNG_BCM_AUDIO_BLOB
+#BOARD_BLUEDROID_VENDOR_CONF := device/samsung/zanin/libbt_vndcfg.txt
+#Assert
+TARGET_OTA_ASSERT_DEVICE := zanin
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_KERNELIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 939524096
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2236070912
+BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+TARGET_PREBUILT_KERNEL := device/samsung/zanin/kernel
+###############
+
+ENABLE_WEBGL := true
+
+###########
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+#BOARD_WLAN_DEVICE_REV       := bcm4330
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   := 802_11_ABG
+
+#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
+
+#bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+#BOARD_HAS_LARGE_FILESYSTEM := true
+
+#egl
+BUILD_EMULATOR_OPENGL := true
+BOARD_EGL_CFG := device/samsung/zanin/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Set /system/bin/sh to ash, not mksh, to make sure we can switch back.
+#TARGET_SHELL := ash
+
+# Enable dex-preoptimization to speed up the first boot sequence
+# of an SDK AVD. Note that this operation only works on Linux for now
+ifeq ($(HOST_OS),linux)
+WITH_DEXPREOPT := true
+endif
+
+#audio
+BOARD_USES_ALSA_AUDIO := true
+BRCM_ALSA_LIB_DIR=device/samsung/bcm_common/alsa-lib
+
+#recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_LDPI_RECOVERY := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+
+# UMS
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/dwc_otg/gadget/lun0/file"
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+
+
+#adb
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.adb.enable=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.config.sec_storage=1
+ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mtp,adb
+
---
+++ cm.mk
@@ -0,0 +1,19 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/themes_common.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := zanin
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/zanin/device_zanin.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zanin
+PRODUCT_NAME := cm_zanin
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := GT-B5330
+PRODUCT_MANUFACTURER := Samsung
